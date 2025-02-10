f9be43128f2267c2050175df6c5f7deaf30a6d18
LBOMI <lcil2293@naver.com>
2025-02-10 14:24:50 +0900

    최적화

--- src/components/TaskInput.js
+++ src/components/TaskInput.js
@@ -12,7 +12,6 @@ const TaskInput = ({ addTask }) => {
   };
 
   
-
   return (
     
     <div style={styles.inputContainer}>
@@ -24,17 +23,8 @@ const TaskInput = ({ addTask }) => {
 };
 
 const styles = {
-  container: { maxWidth: "400px", margin: "50px auto", padding: "20px", textAlign: "center" },
-  title: { fontSize: "24px", marginBottom: "20px", color: "#4773EC" },
   inputContainer: { display: "flex", flexDirection: "column", gap: "10px", marginBottom: "20px" },
   input: { padding: "10px", fontSize: "16px", border: "1px solid #ddd", borderRadius: "4px" },
-  addButton: { padding: "10px", backgroundColor: "#A7ABE2", color: "black", border: "none",  borderRadius: "4px", cursor: "pointer" },
-  taskList: { listStyle: "none", padding: "0" },
-  taskItem: {  display: "flex",  justifyContent: "space-between", padding: "10px", borderBottom: "1px solid #eee" },
-  completedTask: { textDecoration: "line-through", color: "gray"},
-  deleteButton: {  marginLeft: "10px", color: "white", border: "none", borderRadius: "4px", cursor: "pointer"},
-  editButton: { color: "black", border: "none", borderRadius: "4px", cursor: "pointer"},
-  saveButton: { border: "none", color: "white", borderRadius: "4px", cursor: "pointer" },
-  editInput: { padding: "5px", fontSize: "16px", border: "1px solid #ddd", borderRadius: "4px" },
+  addButton: { padding: "10px", backgroundColor: "#A7ABE2", color: "black", border: "none",  borderRadius: "4px", cursor: "pointer" }
 };
 export default TaskInput;
--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -52,12 +52,6 @@ const TaskItem = ({ task, toggleComplete, startEditing, deleteTask, editingId, e
 };
 
 const styles = {
-  container: { maxWidth: "400px", margin: "50px auto", padding: "20px", textAlign: "center" },
-  title: { fontSize: "24px", marginBottom: "20px", color: "#4773EC" },
-  inputContainer: { display: "flex", flexDirection: "column", gap: "10px", marginBottom: "20px" },
-  input: { padding: "10px", fontSize: "16px", border: "1px solid #ddd", borderRadius: "4px" },
-  addButton: { padding: "10px", backgroundColor: "#A7ABE2", color: "black", border: "none", borderRadius: "4px", cursor: "pointer" },
-  taskList: { listStyle: "none", padding: "0" },
   taskItem: { display: "flex", justifyContent: "space-between", padding: "10px", borderBottom: "1px solid #eee" },
   completedTask: { textDecoration: "line-through", color: "gray" },
   deleteButton: { marginLeft: "10px", color: "white", border: "none", borderRadius: "4px", cursor: "pointer" },
